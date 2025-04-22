b1e01a8f9ce13d34549b7f0fa90498bf8254a3e2
MateoL17 <mateo.lasso5345@gmail.com>
2025-04-21 20:19:38 -0500

    Version 2 Final

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -1,4 +1,6 @@
+// Se declara la función
 function verificarPromedio() {
+    // Un array o arreglo con el valor de las 5 notas ingresadas
     const notas = [
       parseFloat(document.getElementById('nota1').value),
       parseFloat(document.getElementById('nota2').value),
@@ -7,18 +9,20 @@ function verificarPromedio() {
       parseFloat(document.getElementById('nota5').value)
     ];
   
-    // Validar que todas las notas esten en el rango de 0 a 10
+    // Esta sección de codigo valida que todas las notas esten en el rango de 0 a 10
     for (let i = 0; i < notas.length; i++) {
       if (isNaN(notas[i]) || notas[i] < 0 || notas[i] > 10) {
         document.getElementById('resultado').textContent =
-          `La nota ${i + 1} no es válida. Debe estar entre 0 y 10.`;
+          "La nota ${i + 1} no es válida. Debe estar entre 0 y 10.";
         return;
       }
     }
   
+    // Suma todos los valores con el .reduce y da el promedio dividiendo entre la cantidad de notas ingresadas
     const promedio = notas.reduce((a, b) => a + b, 0) / notas.length;
     let mensaje = `Promedio: ${promedio.toFixed(2)} - `;
   
+    // Y con el valor del promedio se verifica si tienes mas, igual o menos de 7 y menos o igual de 3.5
     if (promedio >= 7) {
       mensaje += "Pasa de año";
     } else if (promedio >= 3.5) {
@@ -27,6 +31,7 @@ function verificarPromedio() {
       mensaje += "Pierde el semestre.";
     }
   
+    // Se imprime el resultado en el documento
     document.getElementById('resultado').textContent = mensaje;
 }
   
